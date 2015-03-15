62b3b3f307d54e37b064d232182a9d6a63f4ea99
ilbcj <ilbcj@hotmail.com>
2015-03-16 07:49:35 +0800

    add app page\privilege page

--- WebRoot/js/main.js
+++ WebRoot/js/main.js
@@ -24,3 +24,19 @@ function errorTip(msg) {
 function warningTip(title, msg) {
 	$.messager.alert(title, msg,'warning');
 }
+
+$.fn.calendar.defaults.weeks = ['日','一','二','三','四','五','六'];
+$.fn.calendar.defaults.months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一', '十二'];
+$.extend($.fn.datetimebox.defaults,{
+	currentText:'今天',
+	closeText:'关闭',
+	okText:'确定'
+});
+
+//	$.fn.datebox.defaults.formatter = function(date){
+//	var y = date.getFullYear();
+//	var m = date.getMonth()+1;
+//	var d = date.getDate();
+//	alert(y+'/'+m+'/'+d);
+//	return y+'/'+m+'/'+d;
+//}
