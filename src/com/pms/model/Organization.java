888c28cb75ddb099f38619322d3abd6c212f8698
ilbcj <ilbcj@hotmail.com>
2015-05-21 18:03:12 +0800

    change table of org

--- src/com/pms/model/Organization.java
+++ src/com/pms/model/Organization.java
@@ -3,44 +3,50 @@ package com.pms.model;
 public class Organization {
 	public final static int ROOTNODEID = 1;
 	
-	private int id;
-	private String name="";
-	private int parent_id;
+//	private int id;
+	private String GA_DEPARTMENT;//id
+	private String UNIT;
+	private String ORG_LEVEL;
+	private String PARENT_ORG;
+	private int DELETE_STATUS;
+	private int DATA_VERSION;
 	private String description;
-	private String uid;
-	private String org_level;
-	private String tstamp;
-	
-	
-	public String getOrg_level() {
-		return org_level;
+	private String LATEST_MOD_TIME;
+	public String getGA_DEPARTMENT() {
+		return GA_DEPARTMENT;
 	}
-	public void setOrg_level(String org_level) {
-		this.org_level = org_level;
+	public void setGA_DEPARTMENT(String gA_DEPARTMENT) {
+		GA_DEPARTMENT = gA_DEPARTMENT;
 	}
-	public String getTstamp() {
-		return tstamp;
+	public String getUNIT() {
+		return UNIT;
 	}
-	public void setTstamp(String tstamp) {
-		this.tstamp = tstamp;
+	public void setUNIT(String uNIT) {
+		UNIT = uNIT;
 	}
-	public String getUid() {
-		return uid;
+	public String getORG_LEVEL() {
+		return ORG_LEVEL;
 	}
-	public void setUid(String uid) {
-		this.uid = uid;
+	public void setORG_LEVEL(String oRG_LEVEL) {
+		ORG_LEVEL = oRG_LEVEL;
 	}
-	public int getId() {
-		return id;
+	public String getPARENT_ORG() {
+		return PARENT_ORG;
 	}
-	public void setId(int id) {
-		this.id = id;
+	public void setPARENT_ORG(String pARENT_ORG) {
+		PARENT_ORG = pARENT_ORG;
 	}
-	public String getName() {
-		return name;
+	public int getDELETE_STATUS() {
+		return DELETE_STATUS;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setDELETE_STATUS(int dELETE_STATUS) {
+		DELETE_STATUS = dELETE_STATUS;
+	}
+	public int getDATA_VERSION() {
+		return DATA_VERSION;
+	}
+	public void setDATA_VERSION(int dATA_VERSION) {
+		DATA_VERSION = dATA_VERSION;
 	}
 	public String getDescription() {
 		return description;
@@ -48,12 +54,12 @@ public class Organization {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-	public int getParent_id() {
-		return parent_id;
+	public String getLATEST_MOD_TIME() {
+		return LATEST_MOD_TIME;
 	}
-	public void setParent_id(int parent_id) {
-		this.parent_id = parent_id;
+	public void setLATEST_MOD_TIME(String lATEST_MOD_TIME) {
+		LATEST_MOD_TIME = lATEST_MOD_TIME;
 	}
+	
 	
 }
