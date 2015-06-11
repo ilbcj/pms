0ba09fbcfd245906fd4f8f954aafe20b326167ad
ilbcj <ilbcj@hotmail.com>
2015-06-12 07:32:51 +0800

    add search service 3
    add parse xml framework

--- src/com/pms/webservice/service/SyncService.java
+++ src/com/pms/webservice/service/SyncService.java
@@ -12,23 +12,85 @@ public abstract class SyncService {
 	private UserAuth ua;
 	private SearchCondition sc;
 	
+	public DataCommonInfo getDci() {
+		return dci;
+	}
+
+	public void setDci(DataCommonInfo dci) {
+		this.dci = dci;
+	}
+
+	public UserAuth getUa() {
+		return ua;
+	}
+
+	public void setUa(UserAuth ua) {
+		this.ua = ua;
+	}
+
+	public SearchCondition getSc() {
+		return sc;
+	}
+
+	public void setSc(SearchCondition sc) {
+		this.sc = sc;
+	}
+
 	public static SyncService getInstance(Element root)
 	{
-		NodeList datasetList = root.getElementsByTagName("DATASET");
-		for(int i=0;i<datasetList.getLength();i++){
-			Element element = (Element)datasetList.item(i); //得到"page"的第i+1组标签
-			String name = element.getAttribute("name");  //获得ID属性
+		SyncService result = null;
+		DataCommonInfo dci = null;
+		UserAuth ua = null;
+		SearchCondition sc = null;
+		try{
 			
-			//
-			//得到标签title的列表
-			NodeList titleList = element.getElementsByTagName("title");
-			//得到"title"的第1组标签，事实上也只有一组标签
-			Element titleElement = (Element)titleList.item(0);
-			//获得title元素的第一个值
-			String title = titleElement.getFirstChild().getNodeValue();
+			NodeList datasetList = root.getElementsByTagName("DATASET");
+			for(int i=0;i<datasetList.getLength();i++) {
+				Element element = (Element)datasetList.item(i); //得到"page"的第i+1组标签
+				String name = element.getAttribute("name"); //获得name属性
+				if("WA_COMMON_010117".equals(name) ) {
+					result = new SyncSearchService();
+				}
+			}
+			
+			if( result != null ) {
+				for(int i=0;i<datasetList.getLength();i++){
+					Element element = (Element)datasetList.item(i); //得到"page"的第i+1组标签
+					String name = element.getAttribute("name");  //获得ID属性
+					if("WA_COMMON_010000".equals(name) ) {
+						dci = parseDataCommonInfo( element );
+						result.setDci(dci);
+					} else if ("WA_COMMON_010001".equals(name) ) {
+						ua = parseUserAuth( element );
+						result.setUa(ua);
+					} else if("WA_COMMON_010117".equals(name) ) {
+						sc = parseSearchCondition( element );
+						result.setSc(sc);
+					}
+				}
+			}
 		}
-		return null;
+		catch( Exception e ) {
+			e.printStackTrace();
+			result = null;
+		}
+		return result;
 	}
 	
+	private static SearchCondition parseSearchCondition(Element element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static UserAuth parseUserAuth(Element element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static DataCommonInfo parseDataCommonInfo(Element element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public abstract String GetResult();
 }
