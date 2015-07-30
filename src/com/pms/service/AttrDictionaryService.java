e5a36b8e46282447d847173835fda176dadbf3ed
wujuan123 <wujuanem@163.com>
2015-07-30 15:44:30 +0800

    add role dictionary

--- src/com/pms/service/AttrDictionaryService.java
+++ src/com/pms/service/AttrDictionaryService.java
@@ -41,7 +41,21 @@ public class AttrDictionaryService {
 	
 	public int QueryAttrDictionaryOfOrg(AttrDefinition criteria, int page,
 			int rows, ArrayList<AttrDictItem> items) throws Exception {
-		criteria.setType(AttrDefinition.ATTRTYPEREORG);
+		criteria.setType(AttrDefinition.ATTRTYPEORG);
+		AttributeDAO dao = new AttributeDAOImpl();
+		List<AttrDefinition> res = dao.GetAttrDefinitions( criteria, page, rows );
+		AttrDictItem attrDictItem = null;
+		for(int i=0; i<res.size(); i++) {
+			attrDictItem = ConvertAttrDefinitonToAttrDictItem(res.get(i));
+			items.add(attrDictItem);
+		}
+		int total = dao.GetAttrDefinitionsCount( criteria );
+		return total;
+	}
+	
+	public int QueryAttrDictionaryOfRole(AttrDefinition criteria, int page,
+			int rows, ArrayList<AttrDictItem> items) throws Exception {
+		criteria.setType(AttrDefinition.ATTRTYPEROLE);
 		AttributeDAO dao = new AttributeDAOImpl();
 		List<AttrDefinition> res = dao.GetAttrDefinitions( criteria, page, rows );
 		AttrDictItem attrDictItem = null;
