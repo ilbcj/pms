065df395fe63beae4775f5f230e1fdad73c91930
ilbcj <ilbcj@hotmail.com>
2015-12-03 21:44:06 +0800

    fix senior user search function
    remove combobox of title and police number ,change to text area

--- src/com/pms/service/PrivilegeManageService.java
+++ src/com/pms/service/PrivilegeManageService.java
@@ -1,3 +1,12 @@
+/**   
+ * @ClassName:     ${PrivilegeManageService}   
+ * @Description:   ${授权管理功能}   
+ * 
+ * @ProductName:   ${中盈集中授权平台}
+ * @author         ${北京中盈网信科技有限公司}  
+ * @version        V1.0     
+ * @Date           ${2014.8.28} 
+*/
 package com.pms.service;
 
 import java.text.SimpleDateFormat;
